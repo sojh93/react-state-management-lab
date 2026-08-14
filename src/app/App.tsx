8931bab87c6373fdb88812fdfe2ff35ab66581f4
JeongHyeonSo <36619557+sojh93@users.noreply.github.com>
2026-08-14 17:50:06 +0900

    기존 앱파일 제거. 새로 재구성.

---
+++ src/app/App.tsx
@@ -0,0 +1,82 @@
+import {
+    Navigate,
+    NavLink,
+    Route,
+    Routes,
+} from "react-router";
+
+import UseStateTodoPage from "../features/use-state/UseStateTodoPage";
+import PlaceHolderPage from "../pages/PlaceHolderPage";
+
+function App() {
+    return (
+        <div className="app">
+            <header className="app">
+                <div>
+                    <h1>React State Management Lab</h1>
+
+                    <p>
+                        같은 Todo를 여러 상태관리 방식으로 구현해봅니다.
+                    </p>
+                </div>
+            </header>
+
+            <nav className="navigation">
+                <NavLink to="/use-state">
+                    useState
+                </NavLink>
+                <NavLink to="/zustand">
+                    Zustand
+                </NavLink>
+                <NavLink to="/redux">
+                    Redux
+                </NavLink>
+                <NavLink to="/jotai">
+                    Jotai
+                </NavLink>
+                <NavLink to="/tanstack-query">
+                    TanStack Query
+                </NavLink>
+            </nav>
+
+            <main className="main">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <Navigate
+                                to="/use-state"
+                                replace
+                            />
+                        }
+                    />
+                    <Route
+                        path="/use-state"
+                        element={<UseStateTodoPage />}
+                    />
+                    <Route 
+                        path="/zustand"
+                        element={
+                            <PlaceHolderPage title="Zustand Todo" />
+                        }
+                    />
+                    <Route 
+                        path="/redux"
+                        element={
+                            <PlaceHolderPage title="Redux Toolkit Todo" />
+                        }
+                    />
+
+                    <Route
+                        path="/tanstack-query"
+                        element={
+                            <PlaceHolderPage title="TanStack Query Todo" />
+                        }
+                    />
+                </Routes>
+            </main>
+        </div>
+    );
+}
+
+export default App;
